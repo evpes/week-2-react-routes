179c4aecf874b2273992e5926329a36c43138c96
Evgeny Pesterev <pistarsan@gmail.com>
2020-08-29 14:37:43 +0500

    change innerText for route /dashboard again again4

--- client/components/profile.js
+++ client/components/profile.js
@@ -7,12 +7,12 @@ const Profile = () => {
   return (
     <div>
       <Header />
+      <div id="title" innerText="Profile">Profile</div>
       <div className="flex items-center justify-center h-screen">
         <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
-          <div id="title" innerText="Profile">Profile
-            <Link to="/dashboard/main">Go To Main</Link>
-            <Link to="/dashboard">Go To Root</Link>
-          </div>
+          <div id="title" innerText="Profile">Profile</div>
+          <Link to="/dashboard/main">Go To Main</Link>
+          <Link to="/dashboard">Go To Root</Link>
         </div>
         <div className="bg-indigo-800 hover:text-red-500 text-white font-bold rounded-lg border shadow-lg p-10">
           {userid}
