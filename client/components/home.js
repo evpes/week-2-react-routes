859a416ecef59b165373ff7a0cdc8d904a2a7043
Evgeny Pesterev <pistarsan@gmail.com>
2020-08-29 13:36:58 +0500

    change innerText for route /dashboard

--- client/components/home.js
+++ client/components/home.js
@@ -10,7 +10,7 @@ const Home = () => {
 
       <div className="flex items-center justify-center h-screen">
         <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
-          <div id="title">Dashboard
+          <div id="title" innerText="Dashboard">
             <Link to="/dashboard/profile/3e9a44f1-4625-47bc-a990-b5cf38a300ab">Go To Profile</Link>
             <Link to="/dashboard/main">Go To Main</Link>
           </div>
